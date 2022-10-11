bc7a8e1806169c543085b2e333bf9b81a52c826a
QuentinDubois <quentin.dubois@etu.univ-cotedazur.fr>
2022-10-11 16:02:17 +0200

    add a cumcumber test 2

---
+++ src/test/java/fr/unice/polytech/cookiefactory/test/RunCucumberTest.java
@@ -0,0 +1,31 @@
+package fr.unice.polytech.cookiefactory.test;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+
+/*import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(value = Cucumber.class)
+@CucumberOptions(plugin = {"pretty"}, features = "src/test/resources/features")
+public class RunCucumberTest { // will run all features found on the classpath
+                               // in the same package as this class
+}
+*/
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "fr.unice.polytech.cookiefactory")
+
+public class RunCucumberTest {
+    // will run all features found on the classpath
+    // in the same package as this class
+}
---
+++ src/test/java/fr/unice/polytech/cookiefactory/test/StockStepdef.java
@@ -0,0 +1,30 @@
+package fr.unice.polytech.cookiefactory.test;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StockStepdef {
+
+    @Given("the stock already constains {int} doses of chocolates")
+    public void the_stock_already_constains_doses_of_chocolates(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+    }
+    @When("the manager adds {int} doses of chocolates")
+    public void the_manager_adds_doses_of_chocolates(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+    }
+    @Then("the stock should contain {int} doses of chocolates")
+    public void the_stock_should_contain_doses_of_chocolates(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @When("the manager asks to remove  {int} doses of chocolates")
+    public void the_manager_asks_to_remove_doses_of_chocolates(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+    }
+    @Then("the withdrawal is refused")
+    public void the_withdrawal_is_refused() {
+        // Write code here that turns the phrase above into concrete actions
+    }
+}
