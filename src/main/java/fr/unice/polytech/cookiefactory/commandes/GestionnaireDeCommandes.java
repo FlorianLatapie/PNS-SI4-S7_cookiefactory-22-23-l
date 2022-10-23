3fd3f06c882890c7bb0eaf61145f87eafca45839
AureliaChabanier <aurelia.chabanier@etu.unice.fr>
2022-10-23 21:58:08 +0200

    Valider une commande délivrée #5

--- src/main/java/fr/unice/polytech/cookiefactory/commandes/GestionnaireDeCommandes.java
+++ src/main/java/fr/unice/polytech/cookiefactory/commandes/GestionnaireDeCommandes.java
@@ -1,5 +1,6 @@
 package fr.unice.polytech.cookiefactory.commandes;
 
+import fr.unice.polytech.cookiefactory.clientelle.Invite;
 import fr.unice.polytech.cookiefactory.commandes.enums.Etat;
 import fr.unice.polytech.cookiefactory.services.ServiceDEnvoi;
 
@@ -11,11 +12,16 @@ public class GestionnaireDeCommandes {
     private ServiceDEnvoi SMSService;
     private final List<Commande> commandes = new ArrayList<>();
 
+    public List<Commande> getCommandes() {
+        return commandes;
+    }
+
     public List<Commande> voirCommandesEnAttenteDeReception() {
         return this.commandes.stream().filter(c -> c.getEtat() == Etat.EN_ATTENTE_DE_RETRAIT).toList();
     }
 
     public void commandeReceptionnee(Commande commande) {
+        this.commandes.remove(commande);
         commande.setEtat(Etat.RECEPTIONNEE);
     }
 
@@ -25,5 +31,9 @@ public class GestionnaireDeCommandes {
 
     public void ajouterCommande(List<Commande> commandes) {
         this.commandes.addAll(commandes);
+    }
+
+    public Commande obtenirCommandeInvite(String prenom, String nom){
+        return this.voirCommandesEnAttenteDeReception().stream().filter(c -> c.getInvite().getInformationClient().getPrenom().equals(prenom) && c.getInvite().getInformationClient().getNom().equals(nom)).findFirst().get();
     }
 }
