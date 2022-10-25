700da7270170cc9d18d4277e7f8b5105c488564a
AureliaChabanier <aurelia.chabanier@etu.unice.fr>
2022-10-25 15:33:49 +0200

    [US] Ajout de la récupération de commandes depuis l'invité #5
    On récupère les commandes selon l'invité qui veut la récupérer

--- src/test/java/fr/unice/polytech/cookiefactory/test/ValiderUneCommandeDelivreeStepdef.java
+++ src/test/java/fr/unice/polytech/cookiefactory/test/ValiderUneCommandeDelivreeStepdef.java
@@ -12,29 +12,30 @@ import io.cucumber.java.fr.Étantdonné;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ValiderUneCommandeDelivreeStepdef {
-    private Commande commande;
-    private GestionnaireDeCommandes gestionnaireDeCommandes;
+    private GestionnaireDeCommandes gestionnaireDeCommandes = new GestionnaireDeCommandes();
+    private Commande commandeEnAttente;
 
     @Étantdonné("une Commande pour {string} {string} en état {string}")
     public void uneCommandePourEnÉtat(String prenom, String nom, String etat) {
-        commande = new Commande(new Invite(nom, prenom));
+        Commande commande = new Commande(new Invite(nom, prenom));
         commande.setEtat(Etat.valueOf(etat));
-        gestionnaireDeCommandes = new GestionnaireDeCommandes();
         gestionnaireDeCommandes.ajouterCommande(commande);
     }
 
-    @Quand("je veux valider la commande de {string}")
-    public void je_veux_valider_la_commande(String nom) {
-        gestionnaireDeCommandes.commandeReceptionnee(commande);
+    @Quand("je veux valider la commande de {string} {string}")
+    public void je_veux_valider_la_commande(String prenom, String nom) {
+        commandeEnAttente = gestionnaireDeCommandes.obtenirCommandeInvite(prenom, nom);
+        gestionnaireDeCommandes.commandeReceptionnee(commandeEnAttente);
     }
 
     @Alors("la commande est enlevée de la liste des commandes en attente de retrait")
     public void laCommandeEstEnlevéeDeLaListeDesCommandesEnAttenteDeRetrait() {
-        assertFalse(gestionnaireDeCommandes.getCommandes().contains(commande));
+        assertFalse(gestionnaireDeCommandes.getCommandes().contains(commandeEnAttente));
+        assertFalse(gestionnaireDeCommandes.getCommandes().isEmpty());
     }
 
     @Et("la commande est marquée comme {string}")
     public void laCommandeEstMarquéeComme(String etatCommande) {
-        assertEquals(commande.getEtat(), Etat.valueOf(etatCommande));
+        assertEquals(commandeEnAttente.getEtat(), Etat.valueOf(etatCommande));
     }
 }
