18e6871f799e982ff9cd33bc9007642ac6c7df4a
QuentinDubois <quentin.dubois@etu.univ-cotedazur.fr>
2022-11-04 16:08:05 +0100

    ajout du calcul de la réduction #12

---
+++ src/test/java/fr/unice/polytech/cookiefactory/test/ReductionStepdefs.java
@@ -0,0 +1,40 @@
+package fr.unice.polytech.cookiefactory.test;
+
+import fr.unice.polytech.cookiefactory.bd.BaseDeDonnees;
+import fr.unice.polytech.cookiefactory.commandes.Commande;
+import fr.unice.polytech.cookiefactory.divers.Prix;
+import fr.unice.polytech.cookiefactory.recette.cookie.Cookie;
+import io.cucumber.java.fr.Alors;
+import io.cucumber.java.fr.Et;
+import io.cucumber.java.fr.Etantdonné;
+import io.cucumber.java.fr.Lorsque;
+
+
+import static org.junit.Assert.*;
+
+public class ReductionStepdefs {
+    private Commande commande;
+    private Cookie cookie;
+    private Prix reduction;
+
+    @Etantdonné("une commande composée de {int} cookies")
+    public void uneCommandeComposéeDeNbCookieCookies(int nbCookie) {
+        commande = new Commande();
+        commande.getPanier().ajouterCookies(cookie, nbCookie);
+    }
+
+    @Lorsque("le système calcule le montant total de la commande")
+    public void leSystèmeCalculeLeMontantTotalDeLaCommande() {
+        reduction = commande.getPrixReduction();
+    }
+
+    @Etantdonné("un cookie nommé {string} \\(reduction)")
+    public void unCookieNomméReduction(String nomCookie) {
+        cookie = BaseDeDonnees.getInstance().getBdCookie().getCookieParNom(nomCookie);
+    }
+
+    @Alors("le prix est égal à {double} €")
+    public void lePrixEstÉgalÀPrixTotalEuros(double prixReduction) {
+        assertEquals(new Prix((int) (prixReduction*100)), reduction);
+    }
+}
