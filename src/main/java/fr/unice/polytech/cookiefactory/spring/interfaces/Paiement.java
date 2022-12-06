c8fa47e787a3c2efe61aa7169884d35fadd3d602
QuentinDubois <quentin.dubois@etu.univ-cotedazur.fr>
2022-12-06 16:46:09 +0100

    remise en état du projet #52

--- src/main/java/fr/unice/polytech/cookiefactory/spring/interfaces/Paiement.java
+++ src/main/java/fr/unice/polytech/cookiefactory/spring/interfaces/Paiement.java
@@ -1,6 +1,9 @@
 package fr.unice.polytech.cookiefactory.spring.interfaces;
 
+import fr.unice.polytech.cookiefactory.acteur.clients.Invite;
+import fr.unice.polytech.cookiefactory.commandes.Commande;
+
 public interface Paiement {
 
-    void payer();
+    Commande payer(Invite invite);
 }
