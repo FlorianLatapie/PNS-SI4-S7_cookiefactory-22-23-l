280a3547fdaa6ebbdd6995afadbd94bc4eb666ad
QuentinDubois <quentin.dubois@etu.univ-cotedazur.fr>
2022-12-17 17:01:13 +0100

    mise à jour du test gestionCookies #51

--- src/test/java/fr/unice/polytech/cookiefactory/test/recette/AccepterUneNouvelleRecetteStepDef.java
+++ src/test/java/fr/unice/polytech/cookiefactory/test/recette/AccepterUneNouvelleRecetteStepDef.java
@@ -1,11 +1,13 @@
 package fr.unice.polytech.cookiefactory.test.recette;
 
 import fr.unice.polytech.cookiefactory.acteur.employe.management.Dirigeant;
+import fr.unice.polytech.cookiefactory.commandes.enums.Etat;
 import fr.unice.polytech.cookiefactory.cuisine.Cuisinier;
 import fr.unice.polytech.cookiefactory.magasin.ChaineDeMagasins;
 import fr.unice.polytech.cookiefactory.magasin.Magasin;
 import fr.unice.polytech.cookiefactory.recette.cookie.Cookie;
 import fr.unice.polytech.cookiefactory.recette.cookie.Recette;
+import fr.unice.polytech.cookiefactory.recette.cookie.factory.SimpleCookieFactory;
 import fr.unice.polytech.cookiefactory.spring.composants.GestionCookies;
 import io.cucumber.java.fr.Alors;
 import io.cucumber.java.fr.Et;
@@ -16,101 +18,45 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
-
+import static fr.unice.polytech.cookiefactory.recette.enums.ValidationCookie.SOUMIS;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @CucumberContextConfiguration
 @SpringBootTest
 public class AccepterUneNouvelleRecetteStepDef {
-    Dirigeant dirigeant;
-    ChaineDeMagasins chaineDeMagasins = ChaineDeMagasins.getInstance();
     @Autowired
     GestionCookies gestionCookies;
-    Magasin magasin;
-    Cuisinier cuisinier;
     Cookie cookie;
-    String nom;
-    String pate;
-    String saveur;
-    String garniture1;
-    String cuisson;
-    String melange;
-    int temps;
 
-    @Étantdonné("un Dirigent")
-    public void un_dirigent() {
-        dirigeant = new Dirigeant("Jean", "Dupont", "jean_dupont@gmail.com", "1234567890", "secret");
-    }
+    String nomCookie;
 
     @Étantdonné("un cookie {string}")
-    public void un_cookie(String string) {
-        nom = string;
+    public void un_cookie(String nomCookie) {
+        SimpleCookieFactory simpleCookieFactory = new SimpleCookieFactory();
+        simpleCookieFactory.setValidationCookie(SOUMIS);
+        cookie = simpleCookieFactory.creerCookie(nomCookie);
     }
 
-    @Étantdonné("avec une {string}")
-    public void avec_une(String string) {
-        pate = string;
-    }
-
-    @Étantdonné("de {string}")
-    public void de(String string) {
-        saveur = string;
-    }
-
-    @Étantdonné("avec les garnitures : {string}")
-    public void avec_les_garnitures(String string) {
-        garniture1 = string;
-    }
-
-    @Étantdonné("une {string}")
-    public void une(String string) {
-        cuisson = string;
-    }
-
-    @Étantdonné("un {string}")
-    public void un(String string) {
-        melange = string;
-    }
-
-    @Étantdonné("un {int} s de préparation")
-    public void un_s_de_préparation(Integer int1) {
-        temps = int1;
-        cookie = new Cookie(nom,
-                new Recette()
-                        .setPate(pate, 10)
-                        .setSaveur(saveur, 10)
-                        .setGarnitures(List.of(garniture1), 10)
-                        .setCuisson(cuisson)
-                        .setMelange(melange)
-                        .setTempsPreparation(temps)
-        );
-    }
-
-    @Étantdonné("un cuisinier qui a soumis un cookie")
+    @Étantdonné("un cuisinier qui soumet un cookie")
     public void un_cuisinier_qui_a_soumis_un_cookie() {
-        chaineDeMagasins = ChaineDeMagasins.getInstance();
-        magasin = chaineDeMagasins.getAllMagasins().get(0);
-        cuisinier = new Cuisinier(magasin.getGestionnaireDeCommandes());
-        cuisinier.soumettreUnCookie(cookie);
+        gestionCookies.ajouterUnCookie(cookie);
     }
 
-    @Quand("le Dirigent regarde les cookies en attente de validation")
-    public void le_dirigent_regarde_les_cookies_en_attente_de_validation() {
-        assertTrue(gestionCookies.getCookiesEnAttente().contains(cookie));
-    }
-
-    @Alors("le Dirigent valide le cookie {string}")
+    @Alors("un dirigeant valide le cookie {string}")
     public void le_dirigent_valide_le_cookie(String string) {
+        nomCookie = string;
         gestionCookies.validerCookie(string);
-        System.out.println("valide cookie");
-        assertTrue(gestionCookies.getCookiesValide().contains(cookie));
     }
 
-    @Et("les magasins sont notifiés de la disponibilité du cookie {string}")
-    public void lesMagasinsSontNotifiésDeLaDisponibilitéDuCookie(String string) {
-        chaineDeMagasins.getAllMagasins().forEach(magasin -> {
-            System.out.println(magasin.getCookiesDuMagasin().getCookies());
-            assertTrue(magasin.getCookiesDuMagasin().getCookies().containsKey(string));
-        });
+    @Alors("le cookie est en attente de validation")
+    public void leCookieEstEnAttenteDeValidation() {
+        Cookie cookie = gestionCookies.getCookieParNom(nomCookie);
+        assertTrue(gestionCookies.getCookiesEnAttente().stream().map(Cookie::getId).toList().contains(cookie.getId()));
+    }
+
+    @Alors("le cookie est accepté")
+    public void leCookieEstAccepté() {
+        Cookie cookie = gestionCookies.getCookieParNom(nomCookie);
+        assertTrue(gestionCookies.getCookiesValide().stream().map(Cookie::getId).toList().contains(cookie.getId()));
     }
 }
--- src/test/resources/features/test/recette/AccepterUneNouvelleRecette.feature
+++ src/test/resources/features/test/recette/AccepterUneNouvelleRecette.feature
@@ -3,17 +3,10 @@
 Fonctionnalité: Accepter un nouveau cookie
 
   Contexte:
-    Étant donné un Dirigent
-    Et  un cookie "Chocokid"
-    Et avec une "Nature"
-    Et de "Chocolat"
-    Et avec les garnitures : "Pépite de chocolat"
-    Et une "MOELLEUX"
-    Et un "GARNI"
-    Et un 60 s de préparation
+    Etant donné un cookie "Chocokid"
 
   Scénario: Accepter un cookie
-    Étant donné un cuisinier qui a soumis un cookie
-    Quand le Dirigent regarde les cookies en attente de validation
-    Alors le Dirigent valide le cookie "Chocokid"
-    Et les magasins sont notifiés de la disponibilité du cookie "Chocokid"
+    Étant donné un cuisinier qui soumet un cookie
+    Alors le cookie est en attente de validation
+    Quand un dirigeant valide le cookie "Chocokid"
+    Alors le cookie est accepté
